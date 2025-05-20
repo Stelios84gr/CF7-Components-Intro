d775544138fd83237a46ae2cc3abfdfd1d90a4a9
Stelios Fridakis <stelios.fridakis@hotmail.com>
2025-05-20 21:01:47 +0300

    feat: new components created

---
+++ src/components/FunctionalComponent.tsx
@@ -0,0 +1,6 @@
+function functionalComponent() {
+    const title = "Is a Functional Component"
+    return <h1 className="text-center mt-12 text-xl font-bold">{title}</h1>
+}
+
+export default functionalComponent;
