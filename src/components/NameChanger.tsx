7c5a73d352ab1089768f73961bdd86121df2b484
Stelios Fridakis <stelios.fridakis@hotmail.com>
2025-05-28 18:00:59 +0300

    feat: name-changer example added

---
+++ src/components/NameChanger.tsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+
+const NameChanger = () => {
+    const [name, setName] = useState("");
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setName(e.target.value);
+    }
+
+    return (
+        <>
+            <h1 className="text-center text-xl pt-4">Hello, {name || "stranger"}</h1>
+            <input
+                type="text"
+                value={name}
+                onChange={handleChange}
+                className="border px-4 py-2"
+            />
+        </>
+    )
+}
+
+export default NameChanger;
