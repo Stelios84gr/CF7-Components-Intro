85f7d2892abf4059b378cb2c8532609a57d6d36c
Stelios Fridakis <stelios.fridakis@hotmail.com>
2025-06-09 20:06:03 +0300

    feat: document title change functionality via useEffect added

--- src/App.tsx
+++ src/App.tsx
@@ -9,12 +9,12 @@ import Layout from "./components/Layout.tsx";
 // import ClassComponentWithState from "./components/ClassComponentWithState.tsx";
 // import FunctionalComponentWithState from "./components/FunctionalComponentWithState.tsx";
 // import Counter from "./components/Counter.tsx";
-// import NameChanger from "./components/NameChanger.tsx";
+import NameChanger from "./components/NameChanger.tsx";
 // import CounterWithMoreStates from "./components/CounterWithMoreStates.tsx";
 // import CounterAdvanced from "./components/CounterAdvanced.tsx";
 // import CounterWithCustomHook from "./components/CounterWithCustomHook.tsx";
 // import CounterAdvancedWithCustomHook from "./components/CounterAdvancedWithCustomHook.tsx";
-import CounterWithReducer from "./components/CounterWithReducer.tsx";
+// import CounterWithReducer from "./components/CounterWithReducer.tsx";
 
 
 function App() {
@@ -34,12 +34,12 @@ function App() {
       {/*<ClassComponentWithState/>*/}
       {/*<FunctionalComponentWithState/>*/}
       {/*<Counter/>*/}
-      {/*<NameChanger />*/}
+      <NameChanger />
       {/*<CounterWithMoreStates />*/}
       {/*<CounterAdvanced />*/}
       {/*<CounterWithCustomHook/>*/}
       {/*<CounterAdvancedWithCustomHook/>*/}
-      <CounterWithReducer/>
+      {/*<CounterWithReducer/>*/}
       </Layout>
     </>
   );
