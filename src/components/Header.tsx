6606cf4c3a1e228d19b70ab8903aa26832c9c67b
Stelios Fridakis <stelios.fridakis@hotmail.com>
2025-06-16 18:43:29 +0300

    feat: replaces anchor element with Link element so that page doesn't reload when Home button is clicked

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,5 @@
 import CodingFactoryLogo from "./CodingFactoryLogo.tsx";
+import {Link} from "react-router";
 
 const Header = () => {
     return (
@@ -6,7 +7,9 @@ const Header = () => {
             <header className="bg-[#782024] fixed w-full">
                 <div className="container mx-auto px-4 flex items-center justify-between">
                     <CodingFactoryLogo/>
-                    <a className="text-white hover:underline-offset-4" href="/">Home</a>
+                    {/*<a className="text-white hover:underline-offset-4" href="/">Home</a>*/}
+                    {/*in order for the page to not reload*/}
+                    <Link className="text-white hover:underline-offset-4" to="/">Home</Link>
                 </div>
             </header>
         </>
